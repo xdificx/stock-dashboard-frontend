e1de5eeb1477d89c7b8e06e4da51a9bba8c896d4
xdificx <yskrawpts10@gmail.com>
2026-04-13 20:06:58 +0900

    Delete src/market.ts

--- src/market.ts
+++
@@ -1,7 +0,0 @@
-import api from "./client";
-import type { MarketIndex } from "@/lib/types";
-
-export async function fetchMarketIndices(): Promise<Record<string, MarketIndex>> {
-  const res = await api.get("/api/market/indices");
-  return res.data;
-}
