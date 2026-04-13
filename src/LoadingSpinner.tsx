3213a56e9fbcf64d7fb9deb012e126e9bad4c125
xdificx <yskrawpts10@gmail.com>
2026-04-13 20:08:28 +0900

    Delete src/LoadingSpinner.tsx

--- src/LoadingSpinner.tsx
+++
@@ -1,9 +0,0 @@
-import { cn } from "@/lib/utils";
-
-export default function LoadingSpinner({ className }: { className?: string }) {
-  return (
-    <div className={cn("flex items-center justify-center py-16", className)}>
-      <div className="w-8 h-8 border-2 border-border border-t-primary rounded-full animate-spin" />
-    </div>
-  );
-}
